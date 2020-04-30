d4e502a54d146824617f5d2c020e776782f43eb8
bartekfrac <32642449+bartekfrac@users.noreply.github.com>
2020-04-30 21:48:57 +0200

    Moving Player

---
+++ Endless Runner/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private CharacterController controller;
+    private Vector3 direction;
+    public float forwardSpeed;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction.z = forwardSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(direction * Time.fixedDeltaTime);
+    }
+}
