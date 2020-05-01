73380954c73f9a7f951f5dc0b98b56650d7c78b1
bartekfrac <32642449+bartekfrac@users.noreply.github.com>
2020-05-01 14:44:02 +0200

    Camera following player

---
+++ Endless Runner/Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform target;
+    private Vector3 offset;
+    void Start()
+    {
+        offset = transform.position - target.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 newPosition = new Vector3(transform.position.x, transform.position.y, offset.z + target.position.z);
+        transform.position = Vector3.Lerp(transform.position,newPosition,10*Time.deltaTime);
+    }
+}
