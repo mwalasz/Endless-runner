048d15305e2d40a86256e1d8b1c84aba01987ce4
bartekfrac <32642449+bartekfrac@users.noreply.github.com>
2020-05-01 13:07:52 +0200

    Player moving lefte and right

--- Endless Runner/Assets/Scripts/PlayerController.cs
+++ Endless Runner/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,12 @@ public class PlayerController : MonoBehaviour
     private CharacterController controller;
     private Vector3 direction;
     public float forwardSpeed;
+
+    //0 - left
+    //1 - middle
+    //2 - right
+    private int desiredLane = 1;
+    public float laneDistance = 4; //distance beetwen two lanes
     void Start()
     {
         controller = GetComponent<CharacterController>();
@@ -17,6 +23,38 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         direction.z = forwardSpeed;
+
+        //Player lane bounds
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            desiredLane++;
+            if (desiredLane == 3)
+                desiredLane = 2;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            desiredLane--;
+            if (desiredLane == -1)
+                desiredLane = 0;
+        }
+
+        //Calculate where we should be next
+        Vector3 targetPosition = transform.position.z * transform.forward
+                               + transform.position.y * transform.up;
+
+        //moving player correctly
+        if (desiredLane == 0)
+        {
+            targetPosition += Vector3.left * laneDistance;
+        }
+        else if (desiredLane == 2)
+        {
+            targetPosition += Vector3.right * laneDistance;
+        }
+
+        transform.position = Vector3.Lerp(transform.position,targetPosition, forwardSpeed);
+
     }
 
     private void FixedUpdate()
