3da967e1de42969a8a5a2e02e80ae2b084a246c0
bartekfrac <32642449+bartekfrac@users.noreply.github.com>
2020-05-02 17:12:07 +0200

    Endless tiles generate

---
+++ Endless Runner/Assets/Scripts/TileManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    public GameObject[] tilePrefabs;
+    public float zSpawn = 0;
+    public float tileLength = 30;
+    public int numberOfTiles = 5;
+    private List<GameObject> activeTiles = new List<GameObject>();
+
+    public Transform playerTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+     for(int i=0; i<numberOfTiles;i++)
+        {
+            if (i == 0)
+                SpawnTile(0);
+            else
+            SpawnTile(UnityEngine.Random.Range(0, tilePrefabs.Length));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerTransform.position.z - 35 > zSpawn - (numberOfTiles * tileLength))
+        {
+            SpawnTile(UnityEngine.Random.Range(0, tilePrefabs.Length));
+            DeleteTile();
+        }
+    }
+
+    public void SpawnTile( int tileIndex)
+    {
+        GameObject go = Instantiate(tilePrefabs[tileIndex], transform.forward * zSpawn, transform.rotation);
+        activeTiles.Add(go);
+        zSpawn += tileLength;
+    }
+
+    private void DeleteTile()
+    {
+        Destroy(activeTiles[0]);
+        activeTiles.RemoveAt(0);
+    }
+}
