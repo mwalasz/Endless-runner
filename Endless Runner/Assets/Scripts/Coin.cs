197a8345eff329977e4ae7f6c37509a4014f297a
bartekfrac <32642449+bartekfrac@users.noreply.github.com>
2020-05-07 17:51:32 +0200

    Coins added

---
+++ Endless Runner/Assets/Scripts/Coin.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(70 * Time.deltaTime, 0, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            PlayerManager.numberOfCoins += 1;
+            Destroy(gameObject);
+        }
+    }
+}
