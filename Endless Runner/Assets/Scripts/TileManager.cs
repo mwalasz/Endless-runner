7eebd7d29824e3fb42be50b06be628608b5e56e5
mwalasz <mwalasz@interia.pl>
2020-05-21 20:35:39 +0200

    Refactoring of TileManager

--- Endless Runner/Assets/Scripts/TileManager.cs
+++ Endless Runner/Assets/Scripts/TileManager.cs
@@ -5,22 +5,23 @@ using UnityEngine;
 
 public class TileManager : MonoBehaviour
 {
+    private List<GameObject> activeTiles = new List<GameObject>();
+    
+    public Transform playerTransform;
     public GameObject[] tilePrefabs;
     public GameObject[] roadsideTilePrefabs;
+
     public float zSpawn = 0;
     public float tileLength = 30;
+    
     public int numberOfTiles = 5;
     public int numberOfRoadsideTiles = 1;
-    private List<GameObject> activeTiles = new List<GameObject>();
-
-    public Transform playerTransform;
-
     int roadsideTilesCreated = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        for(int i=0; i<numberOfTiles;i++)
+        for(int i = 0; i < numberOfTiles; i++)
         {
             if (i == 0)
                 SpawnTile(0);
@@ -28,17 +29,16 @@ public class TileManager : MonoBehaviour
             
             SpawnRoadsideTiles(0);
         }
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(playerTransform.position.z - 35 > zSpawn - (numberOfTiles * tileLength))
+        if (playerTransform.position.z - 35 > zSpawn - (numberOfTiles * tileLength))
         {
             SpawnTile(UnityEngine.Random.Range(0, tilePrefabs.Length));
             SpawnRoadsideTiles(0);
-            DeleteTile();
+            DeleteTiles();
         }
     }
 
@@ -60,14 +60,13 @@ public class TileManager : MonoBehaviour
         roadsideTilesCreated++;
     }
 
-    private void DeleteTile()
+    //removing three tiles at once (main and two from roadsides)
+    private void DeleteTiles()
     {
-        Destroy(activeTiles[0]);
-        Destroy(activeTiles[1]);
-        Destroy(activeTiles[2]);
-        
-        activeTiles.RemoveAt(0);
-        activeTiles.RemoveAt(1);
-        activeTiles.RemoveAt(2);
+        for (int i = 0; i < 2; i++)
+        {
+            Destroy(activeTiles[i]);
+            activeTiles.RemoveAt(i);
+        }
     }
 }
