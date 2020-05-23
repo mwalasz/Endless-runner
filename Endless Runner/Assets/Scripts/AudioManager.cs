ce85211acbe78792eb6cc154d4b40eeca73325df
mwalasz <mwalasz@interia.pl>
2020-05-23 19:18:20 +0200

    Added AudioManager for taking care of sounds

---
+++ Endless Runner/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (var s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.loop = s.isLooped;
+            s.source.name = s.name;
+            s.source.volume = s.volume;
+        }
+
+        PlaySound("MainTheme");
+    }
+
+    public void PlaySound(string name)
+    {
+        sounds.ToList()
+              .Find(x => x.name.Equals(name))
+              .source.Play();
+    }
+}
---
+++ Endless Runner/Assets/Scripts/Sound.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+    public float volume;
+
+    public AudioClip clip;
+    public AudioSource source;
+    
+    public bool isLooped;
+
+}
