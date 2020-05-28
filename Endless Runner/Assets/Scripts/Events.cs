112258e5def30e52824bf151368e73666b20c414
birdman98 <piotrmatras21@gmail.com>
2020-05-28 22:58:54 +0200

    changed way of loading 'HighScores' scene to 'Additive' mode and required UI elements hiding/unhiding

--- Endless Runner/Assets/Scripts/Events.cs
+++ Endless Runner/Assets/Scripts/Events.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Events : MonoBehaviour
 {
+    public Text gameOverText;
+    public Text speedText;
+    public Button replayBtn;
+    public Button quitBtn;
+    public Button highscoreBtn;
 
     public void ReplayGame()
     {
@@ -16,8 +22,27 @@ public class Events : MonoBehaviour
         Application.Quit();
     }
 
+    private void HideUIElements()
+    {
+        this.gameOverText.gameObject.SetActive(false);
+        this.speedText.gameObject.SetActive(false);
+        this.replayBtn.gameObject.SetActive(false);
+        this.quitBtn.gameObject.SetActive(false);
+        this.highscoreBtn.gameObject.SetActive(false);
+    }
+
+    public void UnhideUIElements()
+    {
+        this.gameOverText.gameObject.SetActive(true);
+        this.speedText.gameObject.SetActive(true);
+        this.replayBtn.gameObject.SetActive(true);
+        this.quitBtn.gameObject.SetActive(true);
+        this.highscoreBtn.gameObject.SetActive(true);
+    }
+
     public void HighscoreTable()
     {
-        SceneManager.LoadScene("HighScores");
+        HideUIElements();    
+        SceneManager.LoadScene("HighScores", LoadSceneMode.Additive);
     }
 }
